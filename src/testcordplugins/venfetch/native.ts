aa8e64f2a0e9996eab1836a6bbb38b62b2402f34
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:05:02 +0200

    chore: update src/testcordplugins/venfetch/native.ts

--- src/testcordplugins/venfetch/native.ts
+++ src/testcordplugins/venfetch/native.ts
@@ -1,3 +1,8 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
 export function getMemory() {
     const memory = process.memoryUsage();
