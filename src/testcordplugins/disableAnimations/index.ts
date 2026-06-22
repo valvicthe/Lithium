0e5374698d2fb795500f9ededcb35543f3789c3f
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:04:56 +0200

    chore: update src/testcordplugins/disableAnimations/index.ts

--- src/testcordplugins/disableAnimations/index.ts
+++ src/testcordplugins/disableAnimations/index.ts
@@ -1,20 +1,8 @@
 /*
- * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2022 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
 import definePlugin from "@utils/types";
 import { findAll } from "@webpack";
@@ -25,10 +13,10 @@ export default definePlugin({
     tags: ["Customisation", "Appearance"],
 	authors: [{ name: "S€th", id: 1273447359417942128n }],
 	start() {
-		this.springs = findAll((mod) => (
+		this.springs = findAll(mod => (
 			typeof mod.Globals === "object" && typeof mod.Springs === "object"
 		));
-        
+
 		for (const spring of this.springs) {
 			if (spring.Globals && typeof spring.Globals.assign === "function") {
 				spring.Globals.assign({
