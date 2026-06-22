d2e974e1adb804441379af1db3e9a93148a482b9
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:04:55 +0200

    chore: update src/testcordplugins/channelGallery/components/GalleryGrid.tsx

--- src/testcordplugins/channelGallery/components/GalleryGrid.tsx
+++ src/testcordplugins/channelGallery/components/GalleryGrid.tsx
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import { Button, React, useEffect, useMemo, useRef, useState } from "@webpack/common";
 
 import type { GalleryItem } from "../utils/extractImages";
