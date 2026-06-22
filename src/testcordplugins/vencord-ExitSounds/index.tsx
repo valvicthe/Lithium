0af20396416e32cfe340993f8964f665c40072d1
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:05:01 +0200

    chore: update src/testcordplugins/vencord-ExitSounds/index.tsx

--- src/testcordplugins/vencord-ExitSounds/index.tsx
+++ src/testcordplugins/vencord-ExitSounds/index.tsx
@@ -6,7 +6,7 @@
 
 import { NavContextMenuPatchCallback } from "@api/ContextMenu";
 import { definePluginSettings } from "@api/Settings";
-import { Devs, TestcordDevs } from "@utils/constants";
+import { TestcordDevs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { Constants, Menu, RestAPI, SelectedChannelStore } from "@webpack/common";
 
@@ -81,8 +81,3 @@ export default definePlugin({
         }
     }
 });
-
-
-
-
-
