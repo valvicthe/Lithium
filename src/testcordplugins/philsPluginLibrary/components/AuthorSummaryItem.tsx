9c26a3e3e81ce34e5c3eeba331a840f279eb4299
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:04:58 +0200

    chore: update src/testcordplugins/philsPluginLibrary/components/AuthorSummaryItem.tsx

--- src/testcordplugins/philsPluginLibrary/components/AuthorSummaryItem.tsx
+++ src/testcordplugins/philsPluginLibrary/components/AuthorSummaryItem.tsx
@@ -16,11 +16,12 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { createDummyUser, types, UserSummaryItem } from "../../philsPluginLibrary";
 import { PluginAuthor } from "@utils/types";
 import { User } from "@vencord/discord-types";
 import { useEffect, UserUtils, useState } from "@webpack/common";
 import React from "react";
+
+import { createDummyUser, types, UserSummaryItem } from "../../philsPluginLibrary";
 
 export interface AuthorUserSummaryItemProps extends Partial<React.ComponentProps<types.UserSummaryItem>> {
     authors: PluginAuthor[];
