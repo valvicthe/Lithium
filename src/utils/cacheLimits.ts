d8cebc0e8fa45ea383e10683b0b2242307d5b306
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:07:30 +0200

    chore: update src/utils/cacheLimits.ts

--- src/utils/cacheLimits.ts
+++ src/utils/cacheLimits.ts
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 export let DIFF_CACHE_MAX = 2000;
 export let TRANSLATION_CACHE_MAX = 500;
 export let ZIP_CACHE_MAX = 100;
