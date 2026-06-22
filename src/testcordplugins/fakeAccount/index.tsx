538b6d544deb5f06808c89d8e49c0fc2d6e07135
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:04:56 +0200

    chore: update src/testcordplugins/fakeAccount/index.tsx

--- src/testcordplugins/fakeAccount/index.tsx
+++ src/testcordplugins/fakeAccount/index.tsx
@@ -1,6 +1,6 @@
 /*
- * Equicord, a Discord client mod
- * Copyright (c) 2024 Vendicated and contributors
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
